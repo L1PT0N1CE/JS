1ce22f964a6b8891a818928965721322e0c3f820
L1PT0N1CE <azadkanat@gmail.com>
2025-03-03 14:58:22 +0100

    Update APM Autofill Shiftreport list Loader NEU.js

--- APM Autofill Shiftreport list Loader NEU.js
+++ APM Autofill Shiftreport list Loader NEU.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        APM Autofill Shiftreport list
+// @name        Shiftreport list
 // @version     0.1
 // @description Füllt Textfelder mit vordefinierten Texten und dynamischen Vorschlägen basierend auf der Eingabe.
 // @author      Kanataza
